fa614e54f6269d43ce6e10e213560add8418f561
Casder <casderiopus@gmail.com>
2021-04-13 19:16:28 +0300

    Fix files

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const getName = () => {
-	const name = readlineSync.question('May I have your name? ');
+  const name = readlineSync.question('May I have your name? ');
 
-	return name;
-}
+  return name;
+};
 
 export default getName;
