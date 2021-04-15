ae227ef77dbad962b4bd1873e25a14addb4a0931
Casder <casderiopus@gmail.com>
2021-04-16 02:33:48 +0300

    Add new functional

---
+++ bin/brain-even.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import { getRandom, getName } from '../src/cli,js';
+import readlineSync from 'readline-sync';
+
+let index = 1;
+let key = true;
+
+console.log('Welcome to the Brain Games!');
+
+const name = getName();
+console.log(`Hello, ${name}!`)'
+
+console.log('Answer \"yes\" if the number is even, otherwise answer \"no\".');
+while (key && index <= 3) {
+	const num = getRandom();
+	const dif = num % 2 == 0 ? 'yes' : 'no';
+	const difOther = num % 2 === 0 ? 'no' : 'yes';
+	console.log('Question: ', num);
+
+	const answer = readlineSync.question('Your answer: ');
+	console.log('Your answer: ', answer);
+	if (answer === dif) {
+		console.log('Correct');
+	}
+	else {
+		console.log(`\'${answer}\' is wrong answer ;\(. Correct answer was ${difOther}.`);
+		key = false;
+	}
+
+	index += 1;
+}
+
+console.log(key ? `Congratulations, ${name}!` : `Let\'s try again, ${name}!`);
